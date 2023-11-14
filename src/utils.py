d693983c02842093689560ef029d860c89fdcaf3
pawelgodula <pawel.godula@gmail.com>
2023-11-14 17:04:53 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,17 @@
 import json
+from typing import List, Dict, Tuple
 
-def load_features_and_params(path_to_opt_settings):
+def load_features_and_params(path_to_opt_settings: str) -> Tuple[List[str], Dict] :
+    """
+    Loads the list of unimportant features and optimal LightGBM parameters from specified files.
+    Parameters:
+    - path_to_opt_settings (str): The path to the directory containing the 'unimportant_features.txt' and 
+                                'optimal_lgbm_params.json' files.
+    Returns:
+    tuple: A tuple containing two elements:
+        - unimportant_features (list of str): A list of feature names considered unimportant.
+        - optimal_lgb_params (dict): A dictionary with the optimal LightGBM parameters.
+    """    
     with open(f'{path_to_opt_settings}/unimportant_features.txt', 'r') as file:
         unimportant_features = [line.strip() for line in file.readlines()]
     with open(f'{path_to_opt_settings}/optimal_lgbm_params.json', 'r') as file:
