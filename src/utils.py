0946df735772e0556f29c4372dbf1b83202cc481
pawelgodula <pawel.godula@gmail.com>
2023-11-14 14:14:46 +0100

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,9 @@
+import json
+
+def load_features_and_params(path_to_opt_settings):
+    with open(f'{path_to_opt_settings}/unimportant_features.txt', 'r') as file:
+        unimportant_features = [line.strip() for line in file.readlines()]
+    df.drop(columns=unimportant_features, inplace=True)
+    with open(f'{path_to_opt_settings}/optimal_lgbm_params.json', 'r') as file:
+         optimal_lgb_params = json.load(file)
+    return unimportant_features, optimal_lgb_params
