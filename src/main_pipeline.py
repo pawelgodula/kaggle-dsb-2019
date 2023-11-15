fe280074e1b7a4d79f9816e833398318fe55ab1e
pawelgodula <pawel.godula@gmail.com>
2023-11-15 08:52:18 +0100

    Update main_pipeline.py

--- src/main_pipeline.py
+++ src/main_pipeline.py
@@ -38,7 +38,7 @@ def parse_args():
     parser.add_argument(
         "--sample_rate",
         type=float,
-        default=0.01,
+        default=0.10,
         help="Sample rate for data processing",
     )
     parser.add_argument(
